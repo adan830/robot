1181d1b5cca2c82d800a46eebb67bddcc09a5bd9
OrangE <einsteiner123@gmail.com>
2014-09-11 15:39:25 +0800

    C++代码

---
+++ src/robot/Main.cc
@@ -0,0 +1,14 @@
+#include <muduo/base/Logging.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/EventLoopThread.h>
+using namespace muduo;
+using namespace muduo::net;
+
+int main()
+{
+    EventLoopThread loopThread;
+
+
+    return 0;
+}
+
