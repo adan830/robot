f767b68da1116f5f885be26c38722eeaa6358076
OrangE <einsteiner123@gmail.com>
2014-09-12 18:16:51 +0800

    初始化 Opcodes 一个客户端可以链接 AccountServer 和 Gatewayserver

--- src/robot/Common.h
+++ src/robot/Common.h
@@ -1,10 +1,38 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-typedef unsigned char      uint8;
-typedef unsigned short     uint16;
-typedef unsigned int       uint32;
-typedef unsigned long long uint64;
+#include <sys/types.h>
+#include <map>
+#include <set>
+#include <list>
+#include <vector>
+
+#ifdef __NXCORE_NAMESPACE__
+#define CORE_NAMESPACE_START namespace NXCore{
+#define CORE_NAMESPACE_END };
+#define USING_NXCORE using namespace NXCore;
+#else
+#define CORE_NAMESPACE_START
+#define CORE_NAMESPACE_END 
+#define USING_NXCORE
+#endif
+
+#define MAP_CONTAINER(KEY, ENTRY) std::map<KEY, ENTRY>
+#define MMAP_CONTAINER(KEY, ENTRY) std::multimap<KEY, ENTRY>
+#define VECTOR_CONTAINER(ENTRY) std::vector<ENTRY>
+#define MSET_CONTAINER(ENTRY) std::multiset<ENTRY>
+#define LIST_CONTENTER(ENTRY) std::list<ENTRY>
+#define SET_CONTAINER(ENTRY) std::set<ENTRY, std::less<ENTRY> >
+
+typedef __int8_t    int8;
+typedef __int16_t   int16;
+typedef __int32_t   int32;
+typedef __int64_t   int64;
+
+typedef __uint8_t   uint8;
+typedef __uint16_t  uint16;
+typedef __uint32_t  uint32;
+typedef __uint64_t  uint64;
 
 #include <muduo/net/TcpClient.h>
 
--- src/robot/Main.cc
+++ src/robot/Main.cc
@@ -1,12 +1,16 @@
+#include "Opcodes.h"
 #include "Client.h"
 
 int main(int argc, char** argv) {
+    tableOpcodes.BuildOpcodeList();
+    
     EventLoop loop;
     InetAddress account("192.168.0.243", 6668);
-    InetAddress gateway("192.168.0.243", 6200);
+    InetAddress gateway("192.168.0.243", 6607);
     
-    Client client(&loop, account, gateway, 0, 0, 0, 0);
-
+    Client client(&loop, account, gateway, 1, 10, 1);
+    loop.loop();
+    
     return 0;
 }
 
