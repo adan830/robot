2791ef1816f5935f9554b3fbbdc9f2b24546d924
OrangE <einsteiner123@gmail.com>
2014-11-24 20:31:15 +0800

    删除多余文件

--- src/robot/WorldSession.cc
+++
@@ -1,9 +0,0 @@
-#include "Opcodes.h"
-#include "WorldSession.h"
-
-void WorldSession::run(uint16 Opcode, WorldPacket& recvPacket)
-{
-//    OpcodeHandle const& opHandler = tableOpcodes[Opcode];
-//    (this->*opHandler.handler)(recvPacket);
-}
-
