56d4a7b1cb1bff271c024d0842b8ababd28dff5f
OrangE <einsteiner123@gmail.com>
2014-12-04 16:21:17 +0800

    新增Robot 消息处理类，统一处理服务器返回的消息，把解包过程放到 Handler 中，保持 Robot 的单一性

---
+++ src/robot/Handler.cc
@@ -0,0 +1,97 @@
+#include "Robot.h"
+#include "Handler.h"
+
+void Handler::SetRobot(Robot * const probot)
+{
+    mRobot = probot;
+}
+
+void Handler::Exec(uint32 code, Buffer& recvPacket)
+{
+    const OpcodeHandle Op = tableOpcodes[code];
+    (this->*Op.handler)(recvPacket);
+}
+
+void Handler::HandleNULL(Buffer& recvPacket)
+{
+    LOG_INFO << "Unknow Msg. ";
+}
+
+void Handler::HandlerGuestAccountOpcode(Buffer& recvPacket)
+{
+    string account     = recvPacket.retrieveAsString(ACCOUNT_NAME_SIZE);
+    string password    = recvPacket.retrieveAsString(PASSWORD_SIZE);
+    uint64 uid         = recvPacket.readInt64();
+    string sign        = recvPacket.retrieveAsString(PLAYER_SIGN_SIZE);
+    tableSessions.AddSession(uid, account, sign);
+    mRobot->GatewayConnect();
+}
+
+void Handler::HandlerAllZoneListOpcode(Buffer& recvPacket)
+{
+    int LastID = recvPacket.readInt16();
+    int Count  = recvPacket.readInt16();
+
+    LOG_INFO << "{AllZoneList "
+             << "\tLastID : " << LastID
+             << "\tCount : " << Count << "}";
+    
+    for (int i = 0; i < Count; ++i) {
+        int ZoneID = recvPacket.readInt16();
+        int State  = recvPacket.readInt8();
+        string ZoneName = recvPacket.retrieveAsString(TINYSTR);
+
+        // 机器人连接返回的第一个服务器
+        mRobot->SetDestZone(ZoneID);
+        
+        LOG_INFO << "{ZoneID : " << ZoneID
+                 << "\tState : " << State
+                 << "\tZoneName : " << ZoneName << "}";
+        break;
+    }
+}
+
+void Handler::HandlerRoleListOpcode(Buffer& recvPacket)
+{
+    uint32 target = recvPacket.readInt32();
+    LOG_INFO << "Target : " << target;
+    uint8 count = recvPacket.readInt8();
+    for (int i = 0; i < count; ++i) {
+        uint32 RoleId = recvPacket.readInt32();
+        uint64 CharId = recvPacket.readInt64();
+        string name   = recvPacket.retrieveAsString(PLAYER_NAME_SIZE);
+        uint32 LeadCard = recvPacket.readInt32();
+        LOG_INFO << "RoleId : "   << RoleId
+                 << "\tCharId : " << CharId
+                 << "\tName : "   << name
+                 << "\tLeadCard : " << LeadCard;
+
+        mRobot->SetCharId(CharId);
+        break;
+    }
+
+    // 角色列表为空，申请创角
+    if (count == 0)
+    {
+        mRobot->OpCreateRole();   
+    }
+    else
+    {
+        mRobot->OpChooseRole();
+    }
+}
+
+void Handler::HandlerAccountLoginSMsgOpcode(Buffer& recvPacket)
+{
+    LOG_INFO << "HandlerAccountLoginSMsgOpcode";
+    uint64 accuid = recvPacket.readInt64();
+    recvPacket.readInt8();
+    string sign   = recvPacket.retrieveAsString(PLAYER_SIGN_SIZE);
+
+    LOG_INFO << "accuid : " << accuid << "  sign : " << sign;
+    mRobot->SetSessSign(sign);
+    mRobot->GatewayConnect();
+    mRobot->OpStartInput();
+}
+
+
--- src/robot/Handler.h
+++ src/robot/Handler.h
@@ -6,6 +6,7 @@
 #include "Common.h"
 #include "Codec.h"
 
+class Robot;
 using namespace muduo;
 using namespace muduo::net;
 
@@ -14,11 +15,18 @@ class Handler
   public :
     Handler(){}
     ~Handler(){}
-
-    void Op(Buffer& buffer, std::string& cmdstr);
-    void OpDungeonFight(Buffer& buffer);
+    
+  public :
+    void SetRobot(Robot * const probot);
+    void Exec(uint32 code, Buffer& recvPacket);
+    void HandleNULL(Buffer& recvPacket);
+    void HandlerGuestAccountOpcode(Buffer& recvPacket);
+    void HandlerAllZoneListOpcode(Buffer& recvPacket);
+    void HandlerRoleListOpcode(Buffer& recvPacket);
+    void HandlerAccountLoginSMsgOpcode(Buffer& recvPacket);
     
   private :
+    Robot * mRobot;
     int    p1, p2, p3, p4, p5, p6, p7, p8, p9, p10;
     std::string s1, s2, s3, s4, s5, s6, s7, s8, s9, s10;
 };
